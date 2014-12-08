630dec3656086af5e9d71ca92b07aab38951d4ce
iweating <iweating@indiana.edu>
2014-12-08 15:42:47 -0500

    EOC

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TicTacToe
 {
-    partial class Form1
+    partial class TicTacToe
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnTopLeft = new System.Windows.Forms.Button();
+            this.btnTopRight = new System.Windows.Forms.Button();
+            this.btnTopMiddle = new System.Windows.Forms.Button();
+            this.btnMiddleMiddle = new System.Windows.Forms.Button();
+            this.btnBottomRight = new System.Windows.Forms.Button();
+            this.btnBottomLeft = new System.Windows.Forms.Button();
+            this.btnBottomMiddle = new System.Windows.Forms.Button();
+            this.btnMiddleRight = new System.Windows.Forms.Button();
+            this.btnMiddleLeft = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTopLeft
+            // 
+            this.btnTopLeft.Location = new System.Drawing.Point(40, 26);
+            this.btnTopLeft.Name = "btnTopLeft";
+            this.btnTopLeft.Size = new System.Drawing.Size(50, 50);
+            this.btnTopLeft.TabIndex = 0;
+            this.btnTopLeft.UseVisualStyleBackColor = true;
+            this.btnTopLeft.Click += new System.EventHandler(this.btnTopLeft_Click);
+            // 
+            // btnTopRight
+            // 
+            this.btnTopRight.Location = new System.Drawing.Point(190, 26);
+            this.btnTopRight.Name = "btnTopRight";
+            this.btnTopRight.Size = new System.Drawing.Size(50, 50);
+            this.btnTopRight.TabIndex = 1;
+            this.btnTopRight.UseVisualStyleBackColor = true;
+            this.btnTopRight.Click += new System.EventHandler(this.btnTopRight_Click);
+            // 
+            // btnTopMiddle
+            // 
+            this.btnTopMiddle.Location = new System.Drawing.Point(117, 26);
+            this.btnTopMiddle.Name = "btnTopMiddle";
+            this.btnTopMiddle.Size = new System.Drawing.Size(50, 50);
+            this.btnTopMiddle.TabIndex = 2;
+            this.btnTopMiddle.UseVisualStyleBackColor = true;
+            this.btnTopMiddle.Click += new System.EventHandler(this.btnTopMiddle_Click);
+            // 
+            // btnMiddleMiddle
+            // 
+            this.btnMiddleMiddle.Location = new System.Drawing.Point(117, 95);
+            this.btnMiddleMiddle.Name = "btnMiddleMiddle";
+            this.btnMiddleMiddle.Size = new System.Drawing.Size(50, 50);
+            this.btnMiddleMiddle.TabIndex = 5;
+            this.btnMiddleMiddle.UseVisualStyleBackColor = true;
+            this.btnMiddleMiddle.Click += new System.EventHandler(this.btnMiddleMiddle_Click);
+            // 
+            // btnBottomRight
+            // 
+            this.btnBottomRight.Location = new System.Drawing.Point(190, 176);
+            this.btnBottomRight.Name = "btnBottomRight";
+            this.btnBottomRight.Size = new System.Drawing.Size(50, 50);
+            this.btnBottomRight.TabIndex = 4;
+            this.btnBottomRight.UseVisualStyleBackColor = true;
+            this.btnBottomRight.Click += new System.EventHandler(this.btnBottomRight_Click);
+            // 
+            // btnBottomLeft
+            // 
+            this.btnBottomLeft.Location = new System.Drawing.Point(40, 176);
+            this.btnBottomLeft.Name = "btnBottomLeft";
+            this.btnBottomLeft.Size = new System.Drawing.Size(50, 50);
+            this.btnBottomLeft.TabIndex = 3;
+            this.btnBottomLeft.UseVisualStyleBackColor = true;
+            this.btnBottomLeft.Click += new System.EventHandler(this.btnBottomLeft_Click);
+            // 
+            // btnBottomMiddle
+            // 
+            this.btnBottomMiddle.Location = new System.Drawing.Point(117, 176);
+            this.btnBottomMiddle.Name = "btnBottomMiddle";
+            this.btnBottomMiddle.Size = new System.Drawing.Size(50, 50);
+            this.btnBottomMiddle.TabIndex = 8;
+            this.btnBottomMiddle.UseVisualStyleBackColor = true;
+            this.btnBottomMiddle.Click += new System.EventHandler(this.btnBottomMiddle_Click);
+            // 
+            // btnMiddleRight
+            // 
+            this.btnMiddleRight.Location = new System.Drawing.Point(190, 95);
+            this.btnMiddleRight.Name = "btnMiddleRight";
+            this.btnMiddleRight.Size = new System.Drawing.Size(50, 50);
+            this.btnMiddleRight.TabIndex = 7;
+            this.btnMiddleRight.UseVisualStyleBackColor = true;
+            this.btnMiddleRight.Click += new System.EventHandler(this.btnMiddleRight_Click);
+            // 
+            // btnMiddleLeft
+            // 
+            this.btnMiddleLeft.Location = new System.Drawing.Point(40, 95);
+            this.btnMiddleLeft.Name = "btnMiddleLeft";
+            this.btnMiddleLeft.Size = new System.Drawing.Size(50, 50);
+            this.btnMiddleLeft.TabIndex = 6;
+            this.btnMiddleLeft.UseVisualStyleBackColor = true;
+            this.btnMiddleLeft.Click += new System.EventHandler(this.btnMiddleLeft_Click);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnBottomMiddle);
+            this.Controls.Add(this.btnMiddleRight);
+            this.Controls.Add(this.btnMiddleLeft);
+            this.Controls.Add(this.btnMiddleMiddle);
+            this.Controls.Add(this.btnBottomRight);
+            this.Controls.Add(this.btnBottomLeft);
+            this.Controls.Add(this.btnTopMiddle);
+            this.Controls.Add(this.btnTopRight);
+            this.Controls.Add(this.btnTopLeft);
+            this.Name = "TicTacToe";
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.TicTacToe_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnTopLeft;
+        private System.Windows.Forms.Button btnTopRight;
+        private System.Windows.Forms.Button btnTopMiddle;
+        private System.Windows.Forms.Button btnMiddleMiddle;
+        private System.Windows.Forms.Button btnBottomRight;
+        private System.Windows.Forms.Button btnBottomLeft;
+        private System.Windows.Forms.Button btnBottomMiddle;
+        private System.Windows.Forms.Button btnMiddleRight;
+        private System.Windows.Forms.Button btnMiddleLeft;
     }
 }
 
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -10,11 +10,238 @@ using System.Windows.Forms;
 
 namespace TicTacToe
 {
-    public partial class Form1 : Form
+    public partial class TicTacToe : Form
     {
-        public Form1()
+        int user = 1;
+        List<List<int>> markedSpots = new List<List<int>>();
+
+
+        public TicTacToe()
         {
             InitializeComponent();
+        }
+
+        private void NewGame()
+        {
+            Random random = new Random();
+            this.user = random.Next(1, 3);
+
+            for (int i = 0; i < 9; i++)
+            {
+                markedSpots.Add(new List<int> { 0, 0 });
+                
+            }
+        }
+
+        private void btnTopLeft_Click(object sender, EventArgs e)
+        {
+            if (user == 1)
+            {
+                btnTopLeft.Image = ((System.Drawing.Image)(Properties.Resources.XMark));
+                markedSpots[0][1] = 1;
+                markedSpots[0][0] = 1;
+                WinnerCheck();
+                user = 2;
+            }
+            else
+            {
+                btnTopLeft.Image = ((System.Drawing.Image)(Properties.Resources.OMark));
+                btnTopLeft.Enabled = false;
+                markedSpots[0][1] = 2;
+                markedSpots[0][0] = 1;
+                WinnerCheck();
+                user = 1;
+            }
+            btnTopLeft.Enabled = false;
+        }
+
+        private void btnTopMiddle_Click(object sender, EventArgs e)
+        {
+            if (user == 1)
+            {
+                btnTopMiddle.Image = ((System.Drawing.Image)(Properties.Resources.XMark));
+                markedSpots[1][1] = 1;
+                markedSpots[1][0] = 1;
+                WinnerCheck();
+                user = 2;
+            }
+            else
+            {
+                btnTopMiddle.Image = ((System.Drawing.Image)(Properties.Resources.OMark));
+                markedSpots[1][1] = 2;
+                markedSpots[1][0] = 1;
+                WinnerCheck();
+                user = 1;
+            }
+            btnTopMiddle.Enabled = false;
+        }
+
+        private void btnTopRight_Click(object sender, EventArgs e)
+        {
+            if (user == 1)
+            {
+                btnTopRight.Image = ((System.Drawing.Image)(Properties.Resources.XMark));
+                markedSpots[2][1] = 1;
+                markedSpots[2][0] = 1;
+                WinnerCheck();
+                user = 2;
+            }
+            else
+            {
+                btnTopRight.Image = ((System.Drawing.Image)(Properties.Resources.OMark));
+                markedSpots[2][1] = 2;
+                markedSpots[2][0] = 1;
+                WinnerCheck();
+                user = 1;
+            }
+            btnTopRight.Enabled = false;
+        }
+
+        private void btnMiddleLeft_Click(object sender, EventArgs e)
+        {
+            if (user == 1)
+            {
+                btnMiddleLeft.Image = ((System.Drawing.Image)(Properties.Resources.XMark));
+                markedSpots[3][1] = 1;
+                markedSpots[3][0] = 1;
+                WinnerCheck();
+                user = 2;
+            }
+            else
+            {
+                btnMiddleLeft.Image = ((System.Drawing.Image)(Properties.Resources.OMark));
+                markedSpots[3][1] = 2;
+                markedSpots[3][0] = 1;
+                WinnerCheck();
+                user = 1;
+            }
+            btnMiddleLeft.Enabled = false;
+        }
+
+        private void btnMiddleMiddle_Click(object sender, EventArgs e)
+        {
+            if (user == 1)
+            {
+                btnMiddleMiddle.Image = ((System.Drawing.Image)(Properties.Resources.XMark));
+                markedSpots[4][1] = 1;
+                markedSpots[4][0] = 1;
+                WinnerCheck();
+                user = 2;
+            }
+            else
+            {
+                btnMiddleMiddle.Image = ((System.Drawing.Image)(Properties.Resources.OMark));
+                markedSpots[4][1] = 2;
+                markedSpots[4][0] = 1;
+                WinnerCheck();
+                user = 1;
+            }
+            btnMiddleMiddle.Enabled = false;
+        }
+
+        private void btnMiddleRight_Click(object sender, EventArgs e)
+        {
+            if (user == 1)
+            {
+                btnMiddleRight.Image = ((System.Drawing.Image)(Properties.Resources.XMark));
+                markedSpots[5][1] = 1;
+                markedSpots[5][0] = 1;
+                WinnerCheck();
+                user = 2;
+            }
+            else
+            {
+                btnMiddleRight.Image = ((System.Drawing.Image)(Properties.Resources.OMark));
+                markedSpots[5][1] = 2;
+                markedSpots[5][0] = 1;
+                WinnerCheck();
+                user = 1;
+            }
+            btnMiddleRight.Enabled = false;
+        }
+
+        private void btnBottomLeft_Click(object sender, EventArgs e)
+        {
+            if (user == 1)
+            {
+                btnBottomLeft.Image = ((System.Drawing.Image)(Properties.Resources.XMark));
+                markedSpots[6][1] = 1;
+                markedSpots[6][0] = 1;
+                WinnerCheck();
+                user = 2;
+            }
+            else
+            {
+                btnBottomLeft.Image = ((System.Drawing.Image)(Properties.Resources.OMark));
+                markedSpots[6][1] = 2;
+                markedSpots[6][0] = 1;
+                WinnerCheck();
+                user = 1;
+            }
+            btnBottomLeft.Enabled = false;
+        }
+
+        private void btnBottomMiddle_Click(object sender, EventArgs e)
+        {
+            if (user == 1)
+            {
+                btnBottomMiddle.Image = ((System.Drawing.Image)(Properties.Resources.XMark));
+                markedSpots[7][1] = 1;
+                markedSpots[7][0] = 1;
+                WinnerCheck();
+                user = 2;
+            }
+            else
+            {
+                btnBottomMiddle.Image = ((System.Drawing.Image)(Properties.Resources.OMark));
+                markedSpots[7][1] = 2;
+                markedSpots[7][0] = 1;
+                WinnerCheck();
+                user = 1;
+            }
+            btnBottomMiddle.Enabled = false;
+        }
+
+        private void btnBottomRight_Click(object sender, EventArgs e)
+        {
+            if (user == 1)
+            {
+                btnBottomRight.Image = ((System.Drawing.Image)(Properties.Resources.XMark));
+                markedSpots[8][1] = 1;
+                markedSpots[8][0] = 1;
+                WinnerCheck();
+                user = 2;
+            }
+            else
+            {
+                btnBottomRight.Image = ((System.Drawing.Image)(Properties.Resources.OMark));
+                markedSpots[8][1] = 2;
+                markedSpots[8][0] = 1;
+                WinnerCheck();
+                user = 1;
+            }
+            btnBottomRight.Enabled = false;
+        }
+
+        private void TicTacToe_Load(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void WinnerCheck()
+        {
+            if ((markedSpots[0][1] == user && markedSpots[1][1] == user && markedSpots[2][1] == user) ||
+                (markedSpots[3][1] == user && markedSpots[4][1] == user && markedSpots[5][1] == user) ||
+                (markedSpots[6][1] == user && markedSpots[7][1] == user && markedSpots[8][1] == user) ||
+                (markedSpots[0][1] == user && markedSpots[3][1] == user && markedSpots[6][1] == user) ||
+                (markedSpots[1][1] == user && markedSpots[4][1] == user && markedSpots[7][1] == user) ||
+                (markedSpots[2][1] == user && markedSpots[5][1] == user && markedSpots[8][1] == user) ||
+                (markedSpots[0][1] == user && markedSpots[4][1] == user && markedSpots[8][1] == user) ||
+                (markedSpots[2][1] == user && markedSpots[4][1] == user && markedSpots[6][1] == user) )
+            {
+                MessageBox.Show("Game Won");
+                NewGame();
+            }
         }
     }
 }
